697542ddf52e682f6aff4799528fe30d30d3d40b
john <john@signalsciences.com>
2019-11-08 10:38:34 -0800

    use public folder instead of deprecated static, add meta tags

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -5,7 +5,7 @@ import Document, { Head, Main, NextScript } from "next/document";
 import getConfig from "next/config";
 
 const { publicRuntimeConfig } = getConfig();
-const { BASE_URL, GA_TRACKING_ID } = publicRuntimeConfig;
+const { GA_TRACKING_ID } = publicRuntimeConfig;
 
 /* eslint-disable react/no-danger */
 
@@ -17,6 +17,33 @@ export default class extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="A collection of components designed to simplify fetch state in React"
+          />
+          <meta property="og:title" content="rsrc · simply fetching" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://signalsciences.github.io/rsrc/"
+          />
+          <meta
+            property="og:description"
+            content="A collection of components designed to simplify fetch state in React"
+          />
+          <meta
+            property="og:image"
+            content="https://signalsciences.github.io/rsrc/static/rsrc-og.png"
+          />
+          <meta name="twitter:card" content="summary" />
+          <meta
+            name="twitter:image"
+            content="https://signalsciences.github.io/rsrc/static/rsrc-og.png"
+          />
+          <link
+            rel="shortcut icon"
+            href="https://signalsciences.github.io/rsrc/static/favicon.png"
+          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
@@ -31,7 +58,6 @@ export default class extends Document {
             `
             }}
           />
-          <link rel="shortcut icon" href={`${BASE_URL}/static/favicon.png`} />
         </Head>
         <body>
           <Main />
