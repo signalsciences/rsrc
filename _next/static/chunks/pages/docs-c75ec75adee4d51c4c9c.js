b24cb8c81fa0cd76bfd7c9d68d62aabb515bdd93
John Donahue <johndonahue@fastly.com>
2022-01-20 11:03:47 -0800

    Updates

---
+++ _next/static/chunks/pages/docs-c75ec75adee4d51c4c9c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[172],{363:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/docs",function(){return u(6667)}])}},function(n){n.O(0,[774,888,179],(function(){return _=363,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
